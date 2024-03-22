aae8d4f5ff661cde812f577ec2548c0ea7f5ac93
Danus the Manus <danchap.allan@gmail.com>
2024-03-22 13:27:59 +0000

    Added testing setup for lexer (but no real tests yet).

---
+++ mimgp/lexer/lexer.test.cc
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "mimgp/eval/eval.h"
+
+/*
+ * The 'test' command outputs 'tost', followed by an enumerated and 
+ * newline-separated list of the arguments passed to it.
+ */
+TEST(Lexer, MemeTest) {
+	ASSERT_EQ(1, 0);
+}
